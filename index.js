06e2be1b4315c915b4ecb915bf0c92414e0e47d0
DonaldBilbili <128892540+DonaldBilbili@users.noreply.github.com>
2023-10-11 18:32:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+const canvas = document.getElementById('canvas1');
+const context = canvas.getContext('2d');
+
+context.beginPath();
+context.arc(35,50,8,0,Math.PI*2,true);
+context.fillStyle = "white"
+context.fill();
+context.closePath();
+
+context.beginPath();
+context.arc(115,50,8,0,Math.PI*2,true);
+context.fillStyle = "white"
+context.fill();
+context.closePath();
+
+context.beginPath();
+context.arc(90,30,70,Math.PI*0.30,Math.PI*0.7,false);
+context.strokeStyle = "white"
+context.lineWidth = 5;
+context.stroke();
+context.closePath();
+
+
+
+const canvas1 = document.getElementById('canvas2');
+const context1 = canvas1.getContext('2d');
+
+context1.beginPath();
+context1.arc(35,50,8,0,Math.PI*2,true);
+context1.fillStyle = "white"
+context1.fill();
+context1.closePath();
+context1.beginPath();
+context1.arc(115,50,8,0,Math.PI*2,true);
+context1.fillStyle = "white"
+context1.fill();
+context1.closePath();
+
+context1.beginPath();
+context1.arc(90,30,70,Math.PI*0.30,Math.PI*0.7,false);
+context1.strokeStyle = "white"
+context1.lineWidth = 5;
+context1.stroke();
+context1.closePath();
+
+
+
+const canvas2 = document.getElementById('canvas3');
+const context2 = canvas2.getContext('2d');
+
+context2.beginPath();
+context2.arc(35,50,8,0,Math.PI*2,true);
+context2.fillStyle = "white"
+context2.fill();
+context2.closePath();
+context2.beginPath();
+context2.arc(115,50,8,0,Math.PI*2,true);
+context2.fillStyle = "white"
+context2.fill();
+context2.closePath();
+
+context2.beginPath();
+context2.arc(90,30,70,Math.PI*0.30,Math.PI*0.7,false);
+context2.strokeStyle = "white"
+context2.lineWidth = 5;
+context2.stroke();
+context2.closePath();
+
+
+
+
+const canvas3 = document.getElementById('canvas4');
+const context3 = canvas3.getContext('2d');
+
+context3.beginPath();
+context3.arc(35,50,8,0,Math.PI*2,true);
+context3.fillStyle = "white"
+context3.fill();
+context3.closePath();
+context3.beginPath();
+context3.arc(115,50,8,0,Math.PI*2,true);
+context3.fillStyle = "white"
+context3.fill();
+context3.closePath();
+
+context3.beginPath();
+context3.arc(90,30,70,Math.PI*0.30,Math.PI*0.7,false);
+context3.strokeStyle = "white"
+context3.lineWidth = 5;
+context3.stroke();
+context3.closePath();
